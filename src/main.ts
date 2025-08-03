5545431627367addfb8df75d3a4bd124a5fc1996
g4n3ux <g4n3ux@gmail.com>
2025-08-03 18:34:26 -0500

    base proyect

---
+++ src/main.ts
@@ -0,0 +1,25 @@
+import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { ValidationPipe, Logger } from '@nestjs/common';
+
+async function bootstrap() {
+  const logger = new Logger('Main');
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT') || 3000;
+  const host = configService.get<string>('HOST') || 'localhost';
+
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(port, host);
+  console.log( `App is running on: ${await app.getUrl()}`);
+  logger.log(`Server running on port ${port}`);
+}
+bootstrap();
