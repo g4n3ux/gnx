ee1796f4f121fa4ddfb262e2729640bcc36b7099
g4n3ux <g4n3ux@gmail.com>
2025-09-19 20:31:58 -0500

    corrigiendo la estructura del proyecto

--- src/graphql/imports.ts
+++ src/graphql/imports.ts
@@ -1,5 +1,5 @@
 import { Module } from "@nestjs/common";
-import { GlobalsModule } from "src/globals/globals.module";
+import { GlobalsModule } from "src/graphql/globals/globals.module";
 
 @Module({
     imports: [
