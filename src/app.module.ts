4db2e54361664545866102a371da017591e49846
g4n3ux <g4n3ux@gmail.com>
2025-09-19 19:30:11 -0500

    se crea el modulo de graphql por separado y se add al app.module la conexion global

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,47 @@
+import { ApolloServer } from '@apollo/server';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { ConfigModule } from '@nestjs/config';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+import { ImportsModule } from './graphql/imports';
 
 @Module({
   imports: [
+    // configuración de graphql ysandbox
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: 'schema.gql', // genera el esquema automáticamente
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault()], // habilita playground
+    }),
+
+  
+
+    // Configuración global de variables de entorno
     ConfigModule.forRoot({
-      isGlobal: true,
-    })  
+      isGlobal: true, // permite usar ConfigService en cualquier módulo
+    }),
+
+    // Conexión a MariaDB
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'mariadb',
+        host: config.get<string>('DATABASE_HOST'),
+        port: config.get<number>('DATABASE_PORT'),
+        username: config.get<string>('DATABASE_USER'),
+        password: config.get<string>('DATABASE_PASSWORD'),
+        database: config.get<string>('DATABASE_NAME'),
+        autoLoadEntities: true,
+        synchronize: true, // ⚠️ en producción mejor usar migraciones
+      }),
+    }),
+
+    // Módulos de la aplicación
+     ImportsModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
---
+++ src/graphql/imports.ts
@@ -0,0 +1,9 @@
+import { Module } from "@nestjs/common";
+import { GlobalsModule } from "src/globals/globals.module";
+
+@Module({
+    imports: [
+        GlobalsModule
+    ]
+})
+export class ImportsModule {}
